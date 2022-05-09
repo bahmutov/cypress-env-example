e710cf41cacd0c97b2e64655840a16216f8287c5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-05-09 16:59:47 -0400

    store screenshots

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,11 +3,15 @@
 it('has valid env values', () => {
   expect(Cypress.env())
     .to.be.an('object')
-    .and.to.have.keys('person', 'location')
+    .and.to.include.keys('person', 'location')
   cy.wrap(Cypress.env('person'))
     .should('have.keys', 'name', 'age')
     .its('age')
     .should('be.within', 10, 99)
   cy.log('**name:** ' + Cypress.env('person').name)
   cy.log('**city:** ' + Cypress.env('location').city)
+
+  // save a screenshot of the test runner
+  // to show what the env values were
+  cy.screenshot('env', { capture: 'runner' })
 })
